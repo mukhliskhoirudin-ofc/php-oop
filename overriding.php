91a81c31d5c43a1bbd6fed41a1bf5389469c8d5c
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-02-19 15:39:34 +0700

    feat: implement overriding in OOP

---
+++ overriding.php
@@ -0,0 +1,82 @@
+<?php
+
+class Produk
+{
+    public $judul, $penulis, $penerbit, $harga;
+
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
+
+        return $str;
+    }
+}
+
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga, $jmlHalaman); //mengunakan overridng, __construct yang atas, tanpa menulis ulang
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman."; //menggunakan overriding untuk memanggil function getInfoProduk
+
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+
+    public $waktuMain;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $waktuMain)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga, $waktuMain);
+
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
+
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Nail Drucmann", "Sony Computer", 35000, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
